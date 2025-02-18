4ebda00b9ad3209b06e42e6f0474deaf5fcec97e
ashleytereseb.31 <ashleytereseb.31@gmail.com>
2025-02-18 15:51:42 +0800

    Added app building s83

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
     const express = require("express");
     const mongoose = require("mongoose");
     const userRoutes = require("./routes/user");
-    const itemRoutes = require("./routes/item")
     const workoutRoutes = require('./routes/workout');
     const auth = require('./auth');
 
